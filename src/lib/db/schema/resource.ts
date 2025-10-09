dad05c96cbce3b850b823a1090b2f23e0e5dbab1
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-10-09 17:32:19 +0700

    feat: add drizzle-zod for enhanced schema validation and create embeddings and resources tables

---
+++ src/lib/db/schema/resource.ts
@@ -0,0 +1,24 @@
+import { boolean, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
+import { nanoid } from 'nanoid'
+
+
+
+export const resources = pgTable("resources", {
+    id: varchar("id").primaryKey().$defaultFn(() => nanoid()).notNull(),
+    fileName : varchar("file_name").notNull(),
+    url: varchar("url").notNull(),
+    type: varchar("type").notNull(),
+    content : varchar("content").notNull(),
+    createdAt: timestamp("created_at")
+    .notNull()
+    .$defaultFn(() => new Date()),
+    updatedAt: timestamp("updated_at")
+        .notNull()
+        .$defaultFn(() => new Date()),
+    isActive: boolean("is_active").notNull().default(true),
+});
+
+export const insertResourceSchema = createInsertSchema(resources).extend({}).omit({id: true, createdAt: true, updatedAt: true, isActive: true});
+export type InsertResource = z.infer<typeof insertResourceSchema>;
